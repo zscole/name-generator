52288e1b8a67547be0a0f855c85e61df46c26e36
zscole <zscole@linux.com>
2024-11-10 12:33:11 -0600

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Chinese Name Generator',
   description: 'Generate your own magical Chinese name! A fun and exciting way to discover your Chinese name.',
-  icons: {
-    icon: '/icon.png'
-  }
+
 }
 
 export default function RootLayout({
